f76cda44d933511f8db9197f9df16a8b0f6e7cc3
chohwanhee(lggram) <great1625@gmail.com>
2023-10-18 17:07:37 +0900

    리소스 수집

---
+++ resource_test.py
@@ -0,0 +1,40 @@
+from pico2d import *
+
+
+def handle_events():
+    global running
+
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            running = False
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
+            running = False
+        else:
+            pass
+
+def reset_world():
+    global running
+
+    running = True
+
+
+def update_world():
+    pass
+
+
+def render_world():
+    clear_canvas()
+    update_canvas()
+
+
+open_canvas()
+reset_world()
+while running:
+    handle_events()
+    update_world()
+    render_world()
+
+    delay(0.03)
+
+close_canvas()
