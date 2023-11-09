a9dd07dd8a3359952718fe8d8be90dcd90d2b113
chohwanhee(lggram) <great1625@gmail.com>
2023-11-09 21:31:54 +0900

    231109

--- menu_mode.py
+++ menu_mode.py
@@ -2,8 +2,8 @@ from pico2d import *
 import game_framework
 
 import game_world
+import play_mode
 
-# boy = None
 
 def handle_events():
     events = get_events()
@@ -15,14 +15,14 @@ def handle_events():
         elif event.type == SDL_MOUSEBUTTONUP and event.button == SDL_BUTTON_LEFT:
             mx, my = event.x, 600 - 1 - event.y
             if mx <= 800 and mx >= 600 and my <= 500 and my >= 300:
+                game_framework.change_mode(play_mode)
                 pass
 
 
 def init():
-    running = True
+    global title_image
 
-    # bird = Bird()
-    # game_world.add_object(bird, 1)
+    title_image = load_image('resource/title.png')
 
 
 def finish():
@@ -37,7 +37,7 @@ def update():
 
 def draw():
     clear_canvas()
-    game_world.render()
+    title_image.composite_draw(0, '', 600, 300, 1200, 600)
     update_canvas()
 
 
@@ -47,4 +47,6 @@ def pause():
 
 def resume():
     pass
+
+
 
